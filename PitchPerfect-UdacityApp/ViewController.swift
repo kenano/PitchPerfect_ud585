dc352525a06e9a6962deb4a06fbf5818414d9e31
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-04-12 17:09:20 -0700

    - added images for buttons. - created viewWillAppear which will be overriden to initialize viewcontroller properties.

--- PitchPerfect-UdacityApp/ViewController.swift
+++ PitchPerfect-UdacityApp/ViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+//        stopRecordingButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
