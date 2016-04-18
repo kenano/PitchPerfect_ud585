6e6e05d16d35fc1ba97420fe9a4caf4703a76315
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-04-18 13:31:14 -0700

    - add extension to playsoundsview controller for audio playback/manipulation. - Give each audio manipulation button a Tag id so that each can be identified by ViewController. - Add properties to PlaySoundsViewController used for audio playback/manipulation. - Add playsound function calls based on which button was pressed. - add stopaudio functionality to stopButtonPressed Action. - call configureUI to enable/disable ui buttons based on app state.

--- PitchPerfect-UdacityApp/PlaySoundsViewController.swift
+++ PitchPerfect-UdacityApp/PlaySoundsViewController.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    //path to the recorded file.
-    var recordedAudioURL: NSURL!
-
+    //outlets to buttons.
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
     @IBOutlet weak var rabbitButton: UIButton!
@@ -21,10 +20,30 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    //path to the recorded file.
+    var recordedAudioURL: NSURL!
+    
+    //properties needed to play/maniplulate audio.
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    //the different types of buttons. this enumeration is used to identify which button was pressed in UI.
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //set up AVAudio. this function is included in the Audio extension file.
+        setupAudio()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        //set the status (enabled/disabled) of the buttons. this function is included in the Audio extension file.
+        configureUI(.NotPlaying)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,12 +51,35 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func playSoundForButton(){
+    @IBAction func playSoundForButton(sender: UIButton){
         print("play sound button pressed")
+        
+        //perform audio manipulation on recording based on which button pressed.
+        //playsound method is included in the Audio extension file.
+        switch (ButtonType(rawValue: sender.tag)!) {
+            case .Slow:
+                playSound(rate: 0.5)
+            case .Fast:
+                playSound(rate: 1.5)
+            case .Chipmunk:
+                playSound(pitch: 1000)
+            case .Vader:
+                playSound(pitch: -1000)
+            case .Echo:
+                playSound(echo: true)
+            case .Reverb:
+                playSound(reverb: true)
+        }
+        
+        //enable/disable ui buttons
+        configureUI(.Playing)
     }
     
     @IBAction func stopButtonPressed(){
         print("stop audio button pressed")
+        
+        //stops audio.included in the Audio extension file.
+        stopAudio()
     }
 
     /*
